2c326354f0f434ac356e6192e9db1ec1ede05407
TJ2001 <jung.m.tim@gmail.com>
2016-09-16 11:42:02 -0700

    establish front-end logic base

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,7 +8,8 @@ public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
-
+    Word wordOne = new Word ("Drive", "Verb", "drahyv");
+    Word wordTwo = new Word ("Apex", "Noun", "ey-peks")
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
@@ -47,6 +48,28 @@ public class App {
       Word word = Word.find(Integer.parseInt(request.params(":id")));
       model.put("word", word);
       model.put("template", "templates/word.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/words/:id/definition/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Word word = Word.find(Integer.parseInt(request.params(":id")));
+      model.put("word", word);
+      model.put("template", "templates/new-definition.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/definitions", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Integer wordId = Integer.parseInt(request.queryParams("wordId"));
+      Word word = Word.find(wordId);
+
+      String annotation = request.queryParams("annotation");
+      Definition newDefinition = new Definition(annotation);
+      word.addDefinition`(newDefinition);
+
+      model.put("word", word);
+      response.redirect("/words/" + wordId);
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
